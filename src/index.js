9198c5443b1af211c4fca8d0f56ee79b1e0cc850
ixg6233 <isaiah_glaspie1@homedepot.com>
2019-11-22 14:10:59 -0500

    creating multiple props and having them display at once

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom'
+
 //
 // function Room() {
 //     return (
@@ -16,15 +17,41 @@ import ReactDOM from 'react-dom'
 // ReactDOM.render (<Room />, document.getElementById('root'));
 
 
-class Room extends React.Component {
-    state = {
-        isLit: true
-    }
-    render() {
+// class Room extends React.Component {
+//     state = {
+//         isLit: true
+//     }
+//     render() {
+//         return (
+//             <div className="room">the room is lit</div>
+//         );
+//     }
+// }
+// ReactDOM.render (<Room />, document.getElementById('root'));
+
+//*****new piece to render several items******
+//     function MediaCard(title, body, imageURL) {
+//     return (
+//         <div > <h2>title</h2>
+//             <p>Here is information within the body in paragraph form I suppose</p>
+//         </div>
+//     );
+// }
+// ReactDOM.render (<MediaCard/>, document.querySelector('#root'));
+
+//***new piece adding props****
+    function MediaStuff(props) {    //props= logo, description, imageURL
         return (
-            <div className="room">the room is lit</div>
-        );
+            <div> Below are Title, brief description and logo
+                <h2>Logo: {props.logo} </h2>
+                <p> {props.description}</p>
+                <h3>
+                    THIS IS MY IMAGE
+                    {props.image}
+                </h3>
+            </div>
+        )
     }
-}
-ReactDOM.render (<Room />, document.getElementById('root'));
+    ReactDOM.render(<MediaStuff logo="EPC" description="a whole bunch of stuff up in here" image={<img src="https://f4.bcbits.com/img/a1227523682_16.jpg"/>} /> , document.querySelector('#root'));
+    //ReactDOM.render(<MediaStuff logo="EPC" description="a whole bunch of stuff up in here" imageURL="https://f4.bcbits.com/img/a1227523682_16.jpg"/> , document.querySelector('#root'));
 
