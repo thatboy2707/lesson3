8f23eea0124dac36bf99d7b64a8908205a3d7d35
ixg6233 <isaiah_glaspie1@homedepot.com>
2019-11-25 16:22:06 -0500

    adding stateful items

--- src/index.js
+++ src/index.js
@@ -5,16 +5,44 @@
 // import * as serviceWorker from './serviceWorker';
 // ReactDOM.render(<App />, document.getElementById('root'));
 // serviceWorker.unregister();
+//import React from 'react';
+//import ReactDOM from 'react-dom';
+
+// function Room() {
+//     return (
+//         <div className={'room'}> the room is lit</div>
+//     )
+// }
+// ReactDOM.render(<Room />, document.getElementById('root'));
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+// class Room extends React.Component {
+//     state = {isLit: true}
+//     render() {
+//         return (
+//             <div className={"room"}>the room is lit</div>
+//         );
+//     }
+// }
+// ReactDOM.render(<Room />, document.getElementById('root'));
 
-function Room() {
-    return (
-        <div className={'room'}> the room is lit</div>
-    )
+class Room extends React.Component {
+    state = {isLit: true}
+    flipLight= () => {
+        this.setState({
+            isLit: !this.state.isLit
+        });
+    };
+    render() {
+        return (
+            <div className="room"> the room is {this.state.isLit ? "lit" : "dark"}
+            <br />
+            <button onClick={this.flipLight}>flip</button>
+            </div>
+
+        );
+    }
 }
 ReactDOM.render(<Room />, document.getElementById('root'));
 
-
-//sdlafnsdlfknasdl
